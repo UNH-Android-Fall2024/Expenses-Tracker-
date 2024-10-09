cd52a0b3b3df8b234c072bb89d17446bc4adc65e
Sri Harsha Vardhan Yendru <harshayendru2001@gmail.com>
2024-10-09 15:16:42 -0400

    Completed the Ui of NewUserAccountCreation
    Completed the Ui of NewUserAccountCreation and modified the MainActivity.kt so that it takes us to account creation page and created NewUserRegistration.kt file

--- app/src/main/java/com/unh/expense_tracker/MainActivity.kt
+++ app/src/main/java/com/unh/expense_tracker/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.unh.expense_tracker
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,11 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val createAccountTextView = findViewById<TextView>(R.id.textViewCreateAccount)
+        createAccountTextView.setOnClickListener {
+            val intent = Intent(this, NewUserRegister::class.java)
+            startActivity(intent)
         }
     }
 }
---
+++ app/src/main/java/com/unh/expense_tracker/NewUserRegister.kt
@@ -0,0 +1,11 @@
+package com.unh.expense_tracker
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NewUserRegister : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.new_user_register) // Make sure this matches the name of your layout file
+    }
+}
