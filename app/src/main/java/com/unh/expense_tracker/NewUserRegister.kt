f9253dbc55c0b87f6c60fff57d2814d5f9ae3150
Sri Harsha Vardhan Yendru <harshayendru2001@gmail.com>
2024-10-09 20:33:34 -0400

    Completed Navigating between the pages
    Navigating from the Sign-Up page to the Login page, and from the Reset Password page back to the Login page.

--- app/src/main/java/com/unh/expense_tracker/NewUserRegister.kt
+++ app/src/main/java/com/unh/expense_tracker/NewUserRegister.kt
@@ -1,5 +1,8 @@
 package com.unh.expense_tracker
+
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class NewUserRegister : AppCompatActivity() {
@@ -7,5 +10,12 @@ class NewUserRegister : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.new_user_register)
+
+
+        val backtologinpage = findViewById<TextView>(R.id.backtologin1)
+        backtologinpage.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/unh/expense_tracker/PasswordReset.kt
+++ app/src/main/java/com/unh/expense_tracker/PasswordReset.kt
@@ -1,5 +1,7 @@
 package com.unh.expense_tracker
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class PasswordReset  : AppCompatActivity() {
@@ -7,5 +9,10 @@ class PasswordReset  : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.password_reset)
+        val backtologinpage2 = findViewById<TextView>(R.id.backtologin2)
+        backtologinpage2.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
