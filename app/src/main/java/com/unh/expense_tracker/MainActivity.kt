7c06bb2a15c23616f2cbad17d6b2363be4351a07
Sri Harsha Vardhan Yendru <harshayendru2001@gmail.com>
2024-10-13 20:02:10 -0400

    Modified MainActivity code for all corner cases

--- app/src/main/java/com/unh/expense_tracker/MainActivity.kt
+++ app/src/main/java/com/unh/expense_tracker/MainActivity.kt
@@ -3,32 +3,72 @@ package com.unh.expense_tracker
 import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.unh.expense_tracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val db = Firebase.firestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-        val createAccountTextView = findViewById<TextView>(R.id.textViewCreateAccount)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val createAccountTextView = findViewById<TextView>(R.id.textViewCreateAccount)
         val resetPassword = findViewById<TextView>(R.id.textViewForgotPassword)
+
         createAccountTextView.setOnClickListener {
             val intent = Intent(this, NewUserRegister::class.java)
             startActivity(intent)
         }
-
         resetPassword.setOnClickListener {
             val intent = Intent(this, PasswordReset::class.java)
             startActivity(intent)
         }
+        binding.buttonLogin.setOnClickListener{
+            logintoapp()
+        }
     }
+    private fun logintoapp(){
+        val email = binding.editTextUserName.text.toString().trim()
+        val password = binding.editTextPassword.text.toString().trim()
+        if (email.isEmpty() || password.isEmpty()){
+            Toast.makeText(this,"Please enter all the Fields",Toast.LENGTH_SHORT).show()
+            return
+        }
+        db.collection("App_UsersCredentials")
+            .whereEqualTo("email",email)
+            .get()
+            .addOnSuccessListener { documents ->
+                if(documents.isEmpty){
+                    Toast.makeText(this,"Email Doesnot Exists",Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    val document = documents.first()
+                    val checkpass=document.getString("password")
+
+                    if(checkpass==password){
+                        Toast.makeText(this,"Logged in Successfully",Toast.LENGTH_SHORT).show()
+
+                        binding.editTextUserName.setText("")
+                        binding.editTextPassword.setText("")
+                    }
+                    else{
+                        Toast.makeText(this,"Incorrect Password",Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            .addOnFailureListener { exception ->
+                Toast.makeText(this, "Error checking email: ${exception.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
 }
