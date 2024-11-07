a971d50e113e9faf7355c21260c9eb3e92466b03
Sri Harsha Vardhan Yendru <harshayendru2001@gmail.com>
2024-11-07 10:02:16 -0500

    Navigating back from expense limit to activity page

--- app/src/main/java/com/unh/expense_tracker/ui/activity/SetExpenseFragment.kt
+++ app/src/main/java/com/unh/expense_tracker/ui/activity/SetExpenseFragment.kt
@@ -2,9 +2,14 @@ package com.unh.expense_tracker.ui.activity
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.unh.expense_tracker.databinding.SetExpenseBinding
@@ -21,5 +26,25 @@ class SetExpenseFragment : Fragment() {
     ): View {
         binding = SetExpenseBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        requireActivity().addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                // abstract method
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when (menuItem.itemId) {
+                    android.R.id.home -> {
+                        requireActivity().onBackPressedDispatcher.onBackPressed()
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
     }
