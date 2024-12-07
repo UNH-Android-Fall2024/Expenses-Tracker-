63abb324b327cdd9cce47e72893859c262c15a00
Sri Harsha Vardhan Yendru <harshayendru2001@gmail.com>
2024-12-07 11:49:35 -0500

    Modified button to image button in expense recycler list

--- app/src/main/java/com/unh/expense_tracker/ui/activity/ExpenseAdapter.kt
+++ app/src/main/java/com/unh/expense_tracker/ui/activity/ExpenseAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
@@ -22,16 +23,15 @@ class ExpenseAdapter(
         val mTextView2: TextView = itemView.findViewById(R.id.text_view2)
         val mTextView3: TextView = itemView.findViewById(R.id.text_view3)
         val mTextView4: TextView = itemView.findViewById(R.id.text_view4)
-        val deletebutt: Button = itemView.findViewById(R.id.buttonDelete)
+        val deletebutt: ImageButton = itemView.findViewById(R.id.buttonDelete)
     }
 
-    // Inflate item layout and create a ViewHolder instance
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExpenseViewHolder {
         val itemView=LayoutInflater.from(parent.context).inflate(R.layout.item_expenses,parent,false)
         return ExpenseViewHolder(itemView)
     }
 
-    // Bind data to the views in the ViewHolder
+
     override fun onBindViewHolder(holder: ExpenseViewHolder, position: Int) {
         val currentItem = mChildList[position]
         holder.mTextView1.text = currentItem.text1
@@ -47,7 +47,6 @@ class ExpenseAdapter(
         }
     }
 
-    // Return the total number of items in the list
     override fun getItemCount(): Int {
         return mChildList.size
     }
